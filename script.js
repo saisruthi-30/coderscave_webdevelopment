71afb2a01e41ff2a1e23660e1413e9869a8062b3
saisruthi-30 <154861150+saisruthi-30@users.noreply.github.com>
2024-01-09 21:37:29 +0530

    Added files related to Port-Folio Project

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var downloadResumeLink = document.getElementById('downloadResumeLink');
+
+    downloadResumeLink.addEventListener('click', function () {
+        // Your code to handle the click event of the "DOWNLOAD RESUME" link
+        // For example, you can open the resume or perform some other action.
+        window.open('main.html', '_blank');
+    });
+});
